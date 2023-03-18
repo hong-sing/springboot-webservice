2c130b5fd4185b85e8fb095260f86bf2449ce075
KHS <ewoking777@gmail.com>
2023-03-18 16:41:30 +0900

    :sparkles: 배포 자동화 구성

--- src/main/java/com/ewok/study/springboot/Application.java
+++ src/main/java/com/ewok/study/springboot/Application.java
@@ -2,6 +2,7 @@ package com.ewok.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @SpringBootApplication 스프링부트의 자동설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
@@ -9,8 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication // @SpringBootApplication이 있는 위치부터 설정을 읽기 때문에 항상 프로젝트의 최상단에 위치 해야함
 public class Application {  // 프로젝트의 메인 클래스
     public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
         // 내장 WAS를 실행
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        application.run(args);
     }
 }
 
